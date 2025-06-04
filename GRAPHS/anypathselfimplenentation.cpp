8a44babc7c4f5e4c45cf6eae2e91ce231dfbb2b0
Monudhakad1 <monudhakad055@gmail.com>
2025-06-04 12:16:06 +0530

    completed dfs learning s

--- GRAPHS/anypathselfimplenentation.cpp
+++ GRAPHS/anypathselfimplenentation.cpp
@@ -1,8 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  
+vector<list<int>> graph;
+int v; // number of vertices
+unordered_set<int>visited;
 
-    return 0;
+// Flag means bi-Directional
+void add_Edge(int source, int destination ,bool flag)
+{
+     graph[source].push_back(destination);
+     if(flag)
+     graph[destination].push_back(source);
+}
+
+void display()
+{
+     for (int i = 0; i < graph.size(); i++)
+     {
+          cout << i << " -> ";
+          for (auto ele : graph[i])
+          {
+               cout << ele << ",";
+          }
+          cout << "\n";
+     }
+}
+
+
+bool DFS_anyPath(int s , int d){
+     if(s==d) return true;
+     visited.insert(s);
+     for(auto ele : graph[s]){
+          if(!visited.count(ele)){
+               bool result=DFS_anyPath(ele , d);
+               if(result) return true;
+          }
+     }
+     return false;
+}
+
+
+
+
+
+
+int main()
+{
+     // Your code goes here
+     cin >> v;
+     graph.resize(v, list<int>());
+
+     int e;
+
+     cin >> e;
+     while (e--)
+     {
+          int s, d ;
+          cin >> s >> d;
+          add_Edge(s, d ,true);
+     }
+
+     cout << DFS_anyPath(0,6) << endl;
 }
