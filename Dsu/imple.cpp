c9f3f8463aa7cb03316f6c85a30d175ec5ad05cc
Monudhakad1 <monudhakad055@gmail.com>
2025-06-11 15:59:22 +0530

    Implements DSU

--- Dsu/imple.cpp
+++ Dsu/imple.cpp
@@ -1,8 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // Your code goes here
+int find(vector<int> &parent, int x)
+{
+    // this method returns group belongs to x
+    return parent[x] = (parent[x] == x) ? x : find(parent, parent[x]);
+}
+
+void Union(vector<int> &parent, vector<int> &rank, int a, int b)
+{
+    a = find(parent, a);
+    b = find(parent, b);
+
+    if(rank[a]>=rank[b]){
+        rank[a]++;
+        parent[b]=a;
+    }
+    else {
+        rank[b]++;
+        parent[a]=b;
+    }
+}
+
+int main()
+{
 
     return 0;
 }
